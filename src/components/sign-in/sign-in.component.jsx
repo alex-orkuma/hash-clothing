bd1141ea41b75559af00821e072ad9cf62a67b04
alex-orkuma <orkumashi@gmail.com>
2021-04-17 11:20:58 +0100

    implemented sigin with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {SignInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, SignInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -15,10 +15,19 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error)
+        }
+        
+        
   };
 
     handleChange = event => {
@@ -50,7 +59,7 @@ class SignIn extends React.Component{
                 handleChange={this.handleChange}/>
                 </form>
                 <div className='buttons'>
-                     <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton onClick={this.handleSubmit} type='submit'>Sign In</CustomButton>
                      <CustomButton onClick={SignInWithGoogle } isGoogleSignIn>{''}Sign In With Google{''}</CustomButton>
                </div>
             </div>
