ab1620243c097035e383f26b9cb597a815ba2579
alex-orkuma <orkumashi@gmail.com>
2021-04-20 19:16:37 +0100

    added cart-dropdown component and implemented cart-icon toggle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import {Link} from 'react-router-dom'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component'
+import { connect } from 'react-redux'; 
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import { connect } from 'react-redux';
 
-const Header = ({currentUser}) =>(
+const Header = ({currentUser, hidden}) =>(
     <div className='header'>
         <Link className='logo-container' to='/' >
             <Logo className='logo' />
@@ -25,9 +27,16 @@ const Header = ({currentUser}) =>(
                 <Link className='option' to='/signin'>SIGN IN</Link>    
             }
         </div>
+        
+        <CartIcon />
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
     </div>
 )
-const mapStatetoProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
-export default  connect(mapStatetoProps)(Header)
+export default  connect(mapStateToProps)(Header)
