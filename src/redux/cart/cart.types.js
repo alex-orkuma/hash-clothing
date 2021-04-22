03c63c577c4c5c31b475916fb54d25c8bd640b17
alex-orkuma <orkumashi@gmail.com>
2021-04-22 17:57:16 +0100

    updated shop data with new data, updated cartitems to aggreage items quantity

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const cartActionsTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD__ITEM: 'ADD__ITEM'
 }
 
 export default cartActionsTypes;
