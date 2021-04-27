d735e53ca429c26d228529a3024abce5e7df0acb
alex-orkuma <orkumashi@gmail.com>
2021-04-27 22:05:56 +0100

    added additem and remove item functionality to checkout-item

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const cartActionsTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD__ITEM: 'ADD__ITEM'
+    ADD__ITEM: 'ADD__ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
+    REMOVE_ITEM: 'REMOVE_ITEM',
 }
 
 export default cartActionsTypes;
